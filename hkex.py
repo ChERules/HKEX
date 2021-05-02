a5aa20180521a4669e12d08c29ad4810f05ea532
Albert Lam <ChERules@users.noreply.github.com>
2021-05-02 22:57:09 +0800

    Add files via upload
    Extract data from text file instead of from downloaded html file.  Text file was converted from the downloaded html file by striping inline html tags and convert escape sequences.

--- hkex.py
+++ hkex.py
@@ -11,14 +11,10 @@ import linearLeastSquare as lsq
 
 # MAIN Function starts
 
-#
-# DOWNLOADING DAILY QUOTATION WEBPAGES FROM HONG KONG STOCK EXCHANGE
-#
-# create a string of today's date yymmdd format
-now = datetime.datetime.now()
-lst = str(now.year)[-2:]+('00'+str(now.month))[-2:]+('00'+str(now.day))[-2:]
-
-# set up directories to store html and extracted data
+# setting up storage Enviroments
+# hdir : for download html and converted text file
+# ddir : for storing the extracted data
+# adir : for result from processing extracted data
 wdir = os.getcwd()
 hdir = wdir + '/HTML/'
 ddir = wdir + '/Data/'
@@ -26,15 +22,23 @@ adir = wdir + '/Analysis/'
 if not os.path.exists(hdir): os.mkdir(hdir)
 if not os.path.exists(ddir): os.mkdir(ddir)
 
-# Set up the starting date for the quotation wegpage download.
+########## DOWNLOAD WEBPAGE
+#
+# DOWNLOADING DAILY QUOTATION WEBPAGES FROM HONG KONG STOCK EXCHANGE
+# and strip all inline html tags to create a text version of the file
+#
+
+# Setting up Enviroument
+# Read today's date in yymmdd format
+now = datetime.datetime.now()
+lst = str(now.year)[-2:]+('00'+str(now.month))[-2:]+('00'+str(now.day))[-2:]
+
+# Set up the starting date for downloading the quotation wegpage.
+# set default to '190430' then update it in the following order:
 # 1. look for the last downloaded file date in env.text
 # 2. check the list of html files already downloaded to determine the last day.
-# 3. As a last resort set date to "190430" seems webpage before May 2019 is no
-#    longer available.
 fst = '190430'
 os.chdir(hdir)
-
-# the last date of webpage which was downloaded
 if os.path.isfile('env.txt'):
     f = open('env.txt', 'r')
     for line in f:
@@ -43,28 +47,26 @@ if os.path.isfile('env.txt'):
             break
     f.close()
 else:
-    # retrun a list of filename without extention
-    datelist = mf.extlist(hdir, '.html', 'N')
-    if len(datelist) >= 1:
+    # return a list of names of all txt files without the extention
+    datelist = mf.extlist(hdir, '.txt', 'N')
+    # extract the latest from all filename
+    if len(datelist) > 1:
         datelist.sort()
         fst = datelist[len(datelist)-1]
-# lastdate: the date which the webpage was successfully downloaded.
 lastdate = fst
 
-# try to download webpages between the last day and today
+# Download the web page for next and consquence date from HKEX if they exist.
+# convert it into text file; Delete the files that is too small to contain
+# useful data; update lastdate after each successful download
 while fst < lst:
-
+    # advance fst to nextday and setup the URL and filenames
     fst = mf.nextday(fst)
-
     url = "https://www.hkex.com.hk/eng/stat/smstat/dayquot/d"+fst+"e.htm"
-    fname = fst + '.html'
-    tname = fst + '.txt'
-    # download the page if it exists
     if mf.url_is_alive(url):
+        fname = fst + '.html'
+        tname = fst + '.txt'
         print('Downloading : ', fname)
         mf.dnload(url, fname, tname)
-
-        # delete the downloaded file which is too small to contain data
         finfo = os.stat(fname)
         if finfo.st_size < 1024:
             os.remove(fname)
@@ -72,69 +74,71 @@ while fst < lst:
         else:
             lastdate = fst
 
-# record the last date which webpage was downloaded
+# record the last date which webpage was downloaded in env.txt
 f = open('env.txt','w')
 f.write('lastdate:'+lastdate+'\n')
 f.close()
 
+######### EXGTRACT DATA FROM TEXT FILES
 #
-# EXTRACT THE SUMMARY OF THE DAILY ACTIVITY FROM EACH DOWNLOADED WEBPAGE
-# OF EACH STOCK AND SAVE IT IN "quotations.csv"
+# EXTRACT THE SUMMARY OF THE DAILY ACTIVITY FROM EACH TEXTFILES
+# Save the information in "quotations.csv"
 #
 os.chdir(ddir)
-# READ WHAT HAS BEEN DONE PREVIOUSLY TO AVOID REPEATITION
-# retrive a list of files in HTML directory and sorted them
-hfiles = mf.extlist(hdir, '.html', 'Y')
-hfiles.sort()
-
 # create a company database if it doesn't exists
 if not os.path.isfile('company.csv'): mf.crcof()
-
-# create a list of company already in the company.csv file
-clist = []
-cfile = open('company.csv', 'r')
-for line in cfile:
-    if line.startswith('code'): continue
-    co = line.split(',')
-    clist.append(co[0])
-cfile.close()
-
-# list of trading date which result had been read previously
-dlist = dict()
-if os.path.isfile('sessions.csv'):
-    sfile = open('sessions.csv', 'r')
-    for line in sfile:
-        if line.startswith('date'): continue
-        sn = line.split(',')
-        dlist[sn[0]] = sn[2].strip()
-    sfile.close()
-    os.remove('sessions.csv')
-
 # create the quotations.csv file if doesn't exists
 if not os.path.isfile("quotations.csv"):
     fout = open('quotations.csv', 'w')
     fout.write('code,date,tdn,high,low,close,ask,bid,turnover,volume\n')
     fout.close()
 
-# create a new sessions file for this session.
+# READ WHAT HAS BEEN DONE PREVIOUSLY TO AVOID REPEATITION
+# retrive a list of all text files with extention in HTML directory
+# and sort them
+textfiles = mf.extlist(hdir, '.txt', 'Y')
+# remove('env.txt') before the sorting; should improve sorting efficiency
+textfiles.remove('env.txt')
+textfiles.sort()
+# create a list of company already in the company.csv file
+comlist = []
+comfile = open('company.csv', 'r')
+for line in comfile:
+    if line.startswith('code'): continue
+    co = line.split(',')
+    comlist.append(co[0])
+comfile.close()
+
+# dictionary of trading date which result had been read/extracted previously
+dlist = dict()
+if os.path.isfile('sessions.csv'):
+    sfile = open('sessions.csv', 'r')
+    for line in sfile:
+        if line.startswith('date'): continue
+        sn = line.split(',')
+        # dictionary pairing date with the annual session number
+        dlist[sn[0]] = sn[2].strip()
+    sfile.close()
+    os.remove('sessions.csv')
+
+# create a new sessions file for the current run.
 sfile = open('sessions.csv', 'w')
 sfile.write('date,idx,tdnum\n')
 
 # loop throught all the downloaded webpages
-for file in hfiles:
+for file in textfiles:
     # extract the date and its order among all webpages
-    # hfiles already sorted previously
+    # textfiles already sorted previously
     date = file[:6]
-    idx = str(hfiles.index(file))
+    idx = str(textfiles.index(file))
 
     # if page already been read previously, record it and preceed to next page.
     if date in dlist:
         sfile.write(date+','+idx+','+dlist[date]+'\n')
-        continue
     else:
         # extract the data and add it to the "quotations.csv"
         print('Processing : ', file)
-        tdn = mf.read_h(hdir+file, clist)
+        tdn = mf.read_h(hdir+file, comlist)
         # record the files has been processed before move on to next one
         sfile.write(date+','+idx+','+tdn+'\n')
 
@@ -147,14 +151,12 @@ sfile.close()
 range = {'lower':5,'upper':50,'skip':5} # lower, upper and inc
 # gather a list of company which we have data on file
 cinfo = dict()
-cfile = open('company.csv', 'r')
-for line in cfile:
-    if line.startswith('code'):
-        continue
-    else:
-        c = line.split(',')
-        cinfo[c[0]] = c[1]+' ('+c[2]+')'
-cfile.close()
+comfile = open('company.csv', 'r')
+for line in comfile:
+    if line.startswith('code'): continue
+    c = line.split(',')
+    cinfo[c[0]] = c[1]+' ('+c[2]+')'
+comfile.close()
 
 # ask user the stock code of all the companies they are inteested in
 colist = []
